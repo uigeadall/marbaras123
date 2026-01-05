7d3fb74acd2dd9c02ac860f41e4744fa5490c5ed
uigeadall <162350255+uigeadall@users.noreply.github.com>
2026-01-05 13:42:37 +0200

    Revert email domain changes - use original from_email directly

--- ecommerce/utils/sendgrid_backend.py
+++ ecommerce/utils/sendgrid_backend.py
@@ -53,20 +53,12 @@ class SendGridBackend(BaseEmailBackend):
     def _send_email(self, message):
         """Send a single email message via SendGrid API."""
         try:
-            # Use DEFAULT_FROM_EMAIL if from_email is not set
-            from_email = message.from_email or getattr(settings, "DEFAULT_FROM_EMAIL", "support@marbaras.com")
-            
-            # If email contains mail.marbaras.com, try to use marbaras.com instead
-            # But only if we're sure SendGrid has marbaras.com verified
-            # For now, keep mail.marbaras.com if that's what's configured
-            # User needs to verify marbaras.com in SendGrid first, then change DEFAULT_FROM_EMAIL env var
-            
             # Prepare email data for SendGrid API
             email_data = {
                 "personalizations": [{
                     "to": [{"email": email} for email in message.to],
                 }],
-                "from": {"email": from_email},
+                "from": {"email": message.from_email},
                 "subject": message.subject,
             }
             
