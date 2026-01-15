2d246c2b00abcab095f288dc5d05334e8dfc79da
uigeadall <162350255+uigeadall@users.noreply.github.com>
2026-01-15 10:09:11 +0200

    Fix migration: Remove old unique_together before adding new one

--- ecommerce/migrations/0030_add_variant_type_to_productvariant.py
+++ ecommerce/migrations/0030_add_variant_type_to_productvariant.py
@@ -10,22 +10,26 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        # First, remove the old unique_together constraint
+        migrations.AlterUniqueTogether(
+            name='productvariant',
+            unique_together=set(),
+        ),
+        # Add the new variant_type field with default value
         migrations.AddField(
             model_name='productvariant',
             name='variant_type',
             field=models.CharField(choices=[('ring_size', 'Ring Size'), ('zodiac_sign', 'Zodiac Sign'), ('other', 'Other')], db_index=True, default='ring_size', help_text='Type of variant', max_length=20),
         ),
+        # Alter the size field to allow blank/null and increase max_length
         migrations.AlterField(
             model_name='productvariant',
             name='size',
             field=models.CharField(blank=True, help_text='Ring size (for ring variants) or zodiac sign (for zodiac variants)', max_length=20, null=True),
         ),
+        # Add the new unique_together constraint with variant_type
         migrations.AlterUniqueTogether(
             name='productvariant',
             unique_together={('product', 'variant_type', 'size')},
-        ),
-        migrations.RemoveConstraint(
-            model_name='productvariant',
-            name='ecommerce_productvariant_product_id_size_uniq',
         ),
     ]
